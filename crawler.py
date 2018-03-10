2d3fdf913bc3b86aab2effab2d030bda550177ce
Maxim Baskakov <mpbaskakov@gmail.com>
2018-03-10 12:04:54 +0300

    Added parsing of datetime

--- crawler.py
+++ crawler.py
@@ -81,6 +81,7 @@ def start():
 def post(bot, update):
     get_matches = crawler()
     today_matches = {}
+    print(get_matches)
     for match in get_matches:
         if match[0] in today_matches:
             today_matches[match[0]].append(match[1:])
